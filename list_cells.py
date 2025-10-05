901470101d361a4464b31dd8a6554786587661aa
Daniel Wanjala <134260642+MadScie254@users.noreply.github.com>
2025-10-06 01:59:00 +0300

    refactor: remove unused script for listing notebook cells

--- list_cells.py
+++
@@ -1,7 +0,0 @@
-import nbformat
-from pathlib import Path
-
-path = Path(r"c:\Users\MadScie254\Documents\GitHub\Stats_CAT_2.1\MSTA_6102_CAT_Stats.ipynb")
-nb = nbformat.read(path, as_version=4)
-for cell in nb.cells:
-    print(repr(cell.get('id')), cell.cell_type, len(cell.source))
